8a4a47ea5a08777ba508510fcff1e3ad1ad70bff
z.zielinska <z.zielinska@insys.pl>
2021-03-22 09:51:31 +0100

    Vue project initalization

---
+++ Vue/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <button
+      type="button"
+      class="btn"
+      @click="showModal"
+    >
+      Open Modal!
+    </button>
+    <ModalForAdrian v-show="isModalVisible" @close="closeModal"/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueConfetti from 'vue-confetti'
+import ModalForAdrian from './components/ModalForAdrian.vue'
+
+Vue.use(VueConfetti)
+
+export default {
+  name: 'App',
+  components: {
+    ModalForAdrian
+  },
+  data() {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      showModal() {
+        this.isModalVisible = true;
+        this.$confetti.start();
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.btn {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+    padding: 10px
+  }
+</style>
---
+++ Vue/src/components/ModalForAdrian.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          Ola Amigo Adrian!
+        </slot>
+        <button
+          type="button"
+          class="btn-close"
+          @click="close"
+        >
+          x
+        </button>
+      </header>
+      <section class="modal-body">
+        <slot name="body">
+          <img alt="Adrian z dyplomem" src="../assets/adi.png">
+        </slot>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueConfetti from 'vue-confetti'
+
+Vue.use(VueConfetti)
+
+  export default {
+    name: 'ModalForAdrian',
+    methods: {
+      close() {
+        this.$emit('close');
+        this.$confetti.stop();
+      }
+    },
+  };
+</script>
+
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header {
+    padding: 15px;
+    display: flex;
+    font-size: 24px;
+    align-items: center;
+    position: relative;
+    border-bottom: 1px solid #eeeeee;
+    color: #4AAE9B;
+    justify-content: space-between;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    border: none;
+    font-size: 20px;
+    padding: 10px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+</style>
