2e399089af05681cd9683d358aaac99ff9054579
GPS1103 <grzegorz.sot1@gmail.com>
2021-06-15 20:47:05 +0200

    Ready

--- Laravel/laravel/app/Http/Controllers/Controller.php
+++ Laravel/laravel/app/Http/Controllers/Controller.php
@@ -2,12 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User as UserResource;
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return (new UserResource($user))
+            ->additional([
+                'token' => $user->createToken('user-token')->plainTextToken,
+            ]);
+    }
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'error' => ['The provided credentials are incorrect.'],
+            ], 422);
+        }
+
+        $user->tokens()->delete();
+
+        return (new UserResource($user))
+            ->additional([
+                'token' => $user->createToken('user-token')->plainTextToken,
+            ]);
+    }
+    public function checkIfLogged()
+    {
+        $user = Auth::user();
+        return new UserResource($user);
+    }
+
 }
--- Laravel/laravel/routes/api.php
+++ Laravel/laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
+Route::group(['middleware' => 'api'], function () {
+    Route::post('login', [Controller::class, 'login']);
+    Route::post('register', [Controller::class, 'register']);
 
+});
+Route::group(['middleware' => ['api', 'auth:sanctum']], function () {
+    Route::get('check', [Controller::class, 'checkIfLogged']);
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
